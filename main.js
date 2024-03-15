89f6da0ff68bd0a94ade61a6f16649c21e1b396f
Yazan Alqadery <yazanalqadery@gmail.com>
2024-03-16 02:17:58 +0300

    Final commit

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const faqCard = document.querySelectorAll(".faq-card");
+const closeSidebar = document.querySelector(".close-menu");
+const openSidebar = document.querySelector(".menu-bar");
 
 faqCard.forEach((card) => {
   card.addEventListener("click", (e) => {
@@ -15,4 +17,14 @@ faqCard.forEach((card) => {
     icon.classList.toggle("fa-minus");
     icon.classList.toggle("fa-plus");
   });
+});
+
+closeSidebar.addEventListener("click", () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("hidden");
+});
+
+openSidebar.addEventListener("click", () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("hidden");
 });
